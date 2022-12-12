64bcdb291194359b2a52caf7a7676daf53a2814a
fodor.krisztian <fodor.krisztian_18b@bolyai-szakkozep.hu>
2022-12-12 14:32:09 +0100

    kész

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
-        while (1 == 1){
+import java.util.Objects;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        while (true){
+            Scanner txt = new Scanner(System.in);
+            String comm = txt.nextLine();
+
+            if (Objects.equals(comm, "hello")){
+                System.out.println("Hello World");
+            }
+            else if (Objects.equals(comm, "info")){
+                System.out.println("v1");
+            }
+            else if (Objects.equals(comm, "exit")){
+                System.out.println("Bye");
+                Thread.sleep(1000);
+                break;
+            }
         }
     }
 }
